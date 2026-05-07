0bb289f353fe67f97b2eec51b81da97173b97be0
mauroaranzate <mauroaranzate@id.uff.br>
2026-05-07 17:49:35 -0300

    Atualizando script esqueleto para etapa 2

---
+++ extensao-esqueleto-orientações-etapa-2.R
@@ -0,0 +1,198 @@
+# Script para leitura de bancos de dados diversos para geração de um data frame de uma única linha referente as informações do estado do aluno
+
+# Ao receber este script esqueleto colocá-lo no repositório LOCAL Extensao, que deve ter sido clonado do GitHub
+# Enviar o script esqueleto para o repositório REMOTO com o nome extensao-esqueleto.R
+
+# Para realizar as tarefas da ETAPA 1, ABRIR ANTES uma branch de nome SINASC no main de Extensao e ir para ela
+# Após os alunos concluírem a ETAPA 1 a professora orientará fazer o merge into main e depois abrir outro branch. Aguarde...
+
+
+####################################
+# ETAPA 1: BANCO DE DADOS DO SINASC
+####################################
+
+# A ALTERAÇÃO DO SCRIPT ESQUELETO - ETAPA 1 - DEVERÁ SER FEITA DENTRO DA BRANCH SINASC
+
+# Tarefa 1. Leitura do banco de dados do SINASC 2015  com 3017668 linhas e 61 colunas
+# verificar se a leitura foi feita corretamente e a estrutura dos dados
+# nomeie o banco de dados como dados_sinasc
+
+
+# Tarefa 2. Reduzir dados_sinasc apenas para as colunas que serão utilizadas, nomeando este novo banco de dados como dados_sinasc_1
+# as colunas serão 1, 4, 5, 6, 7, 12, 13, 14, 15, 19, 21, 22, 23, 24, 35, 38, 44, 46, 48, 59, 60, 61
+# nomes das respectivas variáveis: CONTADOR, CODMUNNASC, LOCNASC, IDADEMAE, ESTCIVMAE, CODMUNRES, GESTACAO, GRAVIDEZ, PARTO,
+# SEXO, APGAR5, RACACOR, PESO, IDANOMAL, ESCMAE2010, RACACORMAE, SEMAGESTAC, CONSPRENAT, TPAPRESENT, TPROBSON, PARIDADE, KOTELCHUCK
+
+
+# Tarefa 3. Reduzir dados_sinasc_1 apenas para o estado que o aluno irá trabalhar (utilizar os dois primeiros dígitos de CODMUNRES), nomeando este novo banco de dados como dados_sinasc_2
+# Códigos das UF: 11: RO, 12: AC, 13: AM, 14: RR, 15: PA, 16: AP, 17: TO, 21: MA, 22: PI, 23: CE, 24: RN
+# 25: PB, 26: PE, 27: AL, 28: SE, 29: BA, 31: MG, 32: ES, 33: RJ, 35: SP, 41: PR, 42: SC, 43: RS
+# 50: MS, 51: MT, 52: GO, 53: DF 
+
+# observar abaixo o número de nascimentos por UF de residência para certificar-se que seu banco de dados está correto
+# 11: 27918     12: 16980     13: 80097     14: 11412     15: 143657    16: 15750      17: 25110
+# 21: 117564    22: 49253     23: 132516    24: 49099     25: 59089     26: 145024     27: 52257     28: 34917     29: 206655
+# 31: 268305    32: 56941     33: 236960    35: 634026     
+# 41: 160947    42: 97223     43: 148359
+# 50: 44142     51: 56673     52: 100672    53: 46122 
+
+# Exportar o arquivo com o nome dados_sinasc_2.csv
+
+
+# Ao concluir a Tarefa 3 da Etapa 1 commite e envie para o repositório REMOTO o script e dados_sinasc_2.csv com o comentário "Dados do estado UF (coloque o nome da UF) e script de sua obtenção"
+
+
+# Tarefa 4. Verificar em dados_sinasc_2 a frequência das categorias das seguintes variáveis: LOCNASC, ESTCIVMAE, GESTACAO, GRAVIDEZ, PARTO,
+# SEXO, RACACOR, IDANOMAL, ESCMAE2010, RACACORMAE, TPAPRESENT, TPROBSON, PARIDADE, KOTELCHUCK
+
+
+# Tarefa 5. Atribuir para cada variável de dados_sinasc_2 como sendo NA a categoria de "Não informado ou Ignorado", geralmente com código 9
+# KOTELCHUCK = 9 significa "não informado"   TPROBSON = 11 significa "não classificado por falta de informação"
+# veja o dicionário do SINASC para identificar qual o código das categorias de cada variável
+# Em variáveis quantitativas como IDADEMAE, CONSPRENAT, APGAR5 e PESO e SEMAGESTAC verificar se existem valores como 99 para NA
+
+
+# Tarefa 6. Atribuir legendas para as categorias das variáveis qualitativas investigadas na tarefa 4.
+# Exemplo: dados_sinasc_2$KOTELCHUCK = factor(dados_sinasc_2$KOTELCHUCK, levels = c(1,2,3,4,5), 
+# labels = c("Não realizou pré-natal", "Inadequado", "Intermediário", "Adequado",  
+# "Mais que adequado")
+
+# ATENçÃO: 1. Na hora de escrever os labels, somente a primeira letra da palavra é maiúscula. Exemplo para SEXO: Feminino e Masculino
+#          2. Nesta Tarefa 6 não crie novas variáveis no banco de dados
+
+
+# Tarefa 7. Categorizar as variáveis IDADEMAE, PESO e APGAR5 e criar variáveis referentes ao deslocamento materno (peregrinação) e estado civil
+# nova variável: dados_sinasc_2$F_PESO com PESO: < 2500: Baixo peso, >=2500 e < 4000: Peso normal, >= 4000: Macrossomia
+# nova variável dados_sinasc_2$F_IDADE com IDADEMAE: <15, 15-19, 20-24, 25-29, 30-34, 35-39, 40-44, 45-49, 50+
+# nova variável dados_sinasc_2$F_APGAR5 com APGAR5: < 7: Baixo, >= 7: Normal
+# Atenção para casos de NA em IDADEMAE, PESO e APGAR5
+
+# nova variável: dados_sinasc_2$PERIG: Não: CODMUNNASC igual a CODMUNRES, Sim: CODMUNNASC diferente de CODMUNRES
+# nova variável: dados_sinasc_2$ESTCIV: Sem companheiro: ESTCIVMAE 1, 3 ou 4, Com companheiro: ESTCIVMAE 2 ou 5
+# Ao categorizar as variáveis, garantir que sejam transformadas em tipo fator
+
+
+# Tarefa 8. Agregar ao banco de dados_sinasc_2 as informações PESO_P10 e PESO_P90 a partir de Tabela_PIG_Brasil.csv
+# a Tabela PIG informa P10 e P90 dos pesos, de acordo com a idade gestacional
+# criar nova variável referente ao peso, de acordo com a idade gestacional, conforme indicado abaixo
+# nova variável apenas para casos de GRAVIDEZ Única: dados_sinasc_2$F_PIG: PIG: PESO < PESO_P10, AIG: PESO_P10 <= PESO <= PESO_P90, GIG: PESO > PESO_P90
+# Atenção para casos de NA em SEMAGESTAC, PESO ou SEXO. Lembre-se também que em dados_sinasc_2 SEXO está como fator com as categorias Feminino e Masculino.
+
+
+# Tarefas 9 e 10 (reformulada) do script esqueleto:
+
+# Crie um banco de dados, de nome SINASC_UF.csv (Exemplo: SINASC_RJ.csv), contendo as 103 variáveis listadas no arquivo “Variáveis - Projeto - Tarefas 9 e 10 da Etapa 1.pdf”
+
+# O banco final deverá possuir:
+#  • 103 colunas, correspondentes às variáveis especificadas;
+#  • n + 1 linhas, onde:
+#  • n corresponde ao número de municípios distintos da UF em análise
+#  a primeira linha corresponde aos valores agregados para a UF como um todo;
+# as demais linhas correspondem aos municípios da UF.
+# As variáveis devem ser construídas a partir dos microdados do SINASC (dados_sinasc, dados_sinasc_1 e dados_sinasc_2), respeitando os nomes e a ordem especificados.
+
+
+# Tarefa 11: Exporte o banco de dados com o nome SINASC_UF.csv
+
+# Ao terminar a ETAPA 1 commite e envie para o repositório REMOTO com o comentário "Dados da UF e Script Etapa 1"
+# Faça um merge de script de SINASC para main
+
+
+
+##################################
+# ETAPA 2: BANCO DE DADOS DO SIM
+##################################
+# Só inicie esta Etapa quando a professora orientar
+# Altere o script esqueleto nas partes que se refere a ETAPA 2 e envie para o repositório Extensao tendo feito o commite "Esqueleto atualizado na Etapa 2"
+# A partir de main crie a branch SIM
+# ESTANDO NA BRANCH SIM, NÃO ALTERE NADA NO SCRIPT REFERENTE A ETAPA 1 e só insira comandos na ETAPA 2
+# Para realizar as tarefas da ETAPA 2, ABRIR ANTES uma branch de nome SIM no main de Extensao e ir para ela
+
+# Tarefa 1. Leitura do banco de dados Mortalidade_Geral_2015 do SIM 2015 com 1264175 linhas e 87 colunas
+# verificar se a leitura foi feita corretamente e a estrutura dos dados
+# nomeie o banco de dados como dados_sim
+
+
+# Tarefa 2. Reduzir dados_sim apenas para as colunas que serão utilizadas, nomeando este novo banco de dados como dados_sim_1
+# as colunas serão: 1, 3, 4, 8, 9, 10, 11, 14, 17, 35, 36, 37, 47, 77, 84
+# nomes das respectivas variáveis: CONTADOR, TIPOBITO, DTOBITO, DTNASC, IDADE, SEXO, RACACOR, ESC2010, CODMUNRES, TPMORTEOCO, 
+# OBITOGRAV, OBITOPUERP, CAUSABAS, TPOBITOCOR, MORTEPARTO
+
+# Tarefa 3. Reduzir dados_sim_1 apenas para o estado que o aluno irá trabalhar (utilizar os dois primeiros dígitos de CODMUNRES), nomeando este novo banco de dados como dados_sim_2
+# Códigos das UF: 11: RO, 12: AC, 13: AM, 14: RR, 15: PA, 16: AP, 17: TO, 21: MA, 22: PI, 23: CE, 24: RN
+# 25: PB, 26: PE, 27: AL, 28: SE, 29: BA, 31: MG, 32: ES, 33: RJ, 35: SP, 41: PR, 42: SC, 43: RS
+# 50: MS, 51: MT, 52: GO, 53: DF 
+
+# observar abaixo o número de óbitos por UF de residência para certificar-se que seu banco de dados está correto
+# 11: 7948      12: 3517      13: 16675     14: 2091      15: 37365     16: 2946       17: 7402
+# 21: 33666     22: 19366     23: 55258     24: 20153     25: 26422     26: 62556      27: 19756     28: 13453     29: 87083
+# 31: 131274    32: 22332     33: 127714    35: 287645     
+# 41: 70839     42: 37984     43: 82349
+# 50: 15457     51: 17095     52: 38854     53: 11975
+
+# Exportar o arquivo com o nome dados_sim_2.csv
+
+
+# Ao concluir a Tarefa 3 da Etapa 2 commite e envie para o repositório REMOTO o script e dados_sim_2.csv com o comentário "Dados do estado UF (coloque o nome da UF) e script de sua obtenção"
+
+
+# Tarefa 4. Verificar em dados_sim_2 a frequência das categorias das seguintes variáveis: TIPOBITO, SEXO, RACACOR, 
+# TPMORTEOCO, OBITOGRAV, OBITOPUERP, CAUSABAS, TPOBITOCOR, MORTEPARTO
+
+
+# Tarefa 5. Atribuir para cada variável de dados_sim_2 como sendo NA a categoria de "Não informado ou Ignorado", geralmente com código 9
+# veja o dicionário do SIM para identificar qual o código das categorias de cada variável
+# Em variáveis quantitativas como IDADE verificar se existem valores como 99 para NA
+
+
+# Tarefa 6. Atribuir legendas para as categorias das variáveis qualitativas investigadas na tarefa 4.
+# Exemplo: dados_sim_2$TIPOBITO = factor(dados_sim_2$TIPOBITO, levels = c(1,2), 
+# labels = c("Fetal", "Não fetal")
+
+# ATENçÃO: 1. Na hora de escrever os labels, somente a primeira letra da palavra é maiúscula. Exemplo para SEXO: Feminino e Masculino
+#          2. Nesta Tarefa 6 não crie novas variáveis no banco de dados
+
+
+# Tarefa 7. Crie um banco de dados, de nome SIM_UF.csv (Exemplo: SIM_RJ.csv), contendo as 41 variáveis listadas no arquivo “Variáveis - Projeto - Tarefa 7 da Etapa 2.pdf”
+# Atenção:
+# 1. Para informações gerais utilize CAUSABAS, SEXO e IDADE
+# 2. Para informações fetais utilize TIPOBITO
+# 3. Para informações neonatais utilize TIPOBITO não fetal e IDADE entre 0 e 27 dias e RACACOR
+# 4. Para informações maternas utilize TPMORTEOCO, ESC e IDADE
+
+
+# Tarefa 8: Exporte o banco de dados com o nome SIM_UF.csv
+
+# Ao terminar a ETAPA 2 commite e envie para o repositório REMOTO com o comentário "Dados da UF e Script Etapa 2"
+# Faça um merge de script de SIM para main
+
+
+#####################################################
+# ETAPA 3: OUTROS BANCOS DE DADOS: IBGE, SNIS, ...
+#####################################################
+# Só inicie esta Etapa quando a professora orientar
+# ESTANDO NA BRANCH SINASC, NÃO ALTERE NADA NO SCRIPT REFERENTE A ETAPA 3
+
+# Tarefa 1. Acesso aos bancos de dados e obtenção da informação
+
+
+
+#####################################################################################################
+# ETAPA 4: GERAR BANCO DE DADOS FINAL DO ESTADO, BASEADO NAS ANÁLISES DE SINASC, SIM, IBGE, SNIS,...
+######################################################################################################
+# Só inicie esta Etapa quando a professora orientar
+# ESTANDO NA BRANCH SINASC, NÃO ALTERE NADA NO SCRIPT REFERENTE A ETAPA 4
+
+# Cada aluno gerar um dataframe de uma única linha (referente ao seu estado) com as variáveis na ordem indicada pela professora
+
+
+
+############################################################################################
+# ETAPA 5: EMPILHAMENTO DOS DATAFRAMES DE CADA ESTADO, GERANDO UM DATAFRAME DE 27 LINHAS
+############################################################################################
+# Só inicie esta Etapa quando a professora orientar
+# ESTANDO NA BRANCH SINASC, NÃO ALTERE NADA NO SCRIPT REFERENTE A ETAPA 5
+
+# 1. Enviar arquivos para as pastas do repositório da Professora no GitHUb
+# 2. A professora fará o empilhamentos dos dataframes
+
